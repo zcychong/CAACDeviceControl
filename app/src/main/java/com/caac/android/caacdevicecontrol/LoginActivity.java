ea4d366a71811abbd2f0dbce0e8d96c44b09b807
zcychong <zcychong7289610>
2016-07-20 17:03:46 +0800

    忘了

--- app/src/main/java/com/caac/android/caacdevicecontrol/LoginActivity.java
+++ app/src/main/java/com/caac/android/caacdevicecontrol/LoginActivity.java
@@ -3,6 +3,7 @@ package com.caac.android.caacdevicecontrol;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class LoginActivity extends BaseActivity {
     private final String TAG = getClass().getSimpleName();
@@ -20,5 +21,14 @@ public class LoginActivity extends BaseActivity {
     private void initView(){
 
         toolbar = (Toolbar)findViewById(R.id.toolbar);
+//        toolbar.setTitle(R.string.login);
+        toolbar.setNavigationIcon(R.mipmap.icon_back);
+        toolbar.setNavigationContentDescription("标题");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
