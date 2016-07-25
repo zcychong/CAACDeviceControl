d5501b8323ef473c4c28cfb68d50c7ed4e75ee6d
zcychong <zcychong7289610>
2016-07-25 19:45:28 +0800

    UI修改 基本界面

--- app/src/main/java/com/caac/android/caacdevicecontrol/LoginActivity.java
+++ app/src/main/java/com/caac/android/caacdevicecontrol/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.caac.android.caacdevicecontrol;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -9,6 +11,7 @@ public class LoginActivity extends BaseActivity {
     private final String TAG = getClass().getSimpleName();
     private Activity context;
 
+    private AppCompatButton acbtnLogin;
     private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +23,22 @@ public class LoginActivity extends BaseActivity {
 
     private void initView(){
 
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
-//        toolbar.setTitle(R.string.login);
-        toolbar.setNavigationIcon(R.mipmap.icon_back);
-        toolbar.setNavigationContentDescription("标题");
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//        toolbar = (Toolbar)findViewById(R.id.toolbar);
+////        toolbar.setTitle(R.string.login);
+//        toolbar.setNavigationIcon(R.mipmap.icon_back);
+//        toolbar.setNavigationContentDescription("标题");
+//        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                finish();
+//            }
+//        });
+
+        acbtnLogin = (AppCompatButton)findViewById(R.id.btn_login);
+        acbtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                startActivity(new Intent(context, MainActivity.class));
             }
         });
     }
